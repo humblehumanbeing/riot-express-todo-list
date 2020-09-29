afe7559c938a9b8a158dab7d55f02af8cd77c430
Suat Ural <sural@windowslive.com>
2020-09-29 12:16:08 -0400

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.234.91.236',
+      host: '52.91.221.175',
       username: 'ubuntu',
       privateKey: '../UbuntuEC2.pem'
     })
